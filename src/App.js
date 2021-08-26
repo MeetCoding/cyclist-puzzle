bf5d80a3fa907db849482b897534f9a174f3da98
MeetCoding <meet.prakash.shah@gmail.com>
2021-08-26 13:33:37 +0530

    created non responsive desing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import board from './board.svg';
 import wheel from './wheel.svg';
 
 function App() {
+
+  const [wheelStyle, setWheelStyle] = useState({ transform: 'rotate(0deg)' });
+
+  function rotateWheel() {
+    wheelStyle.transform === 'rotate(0deg)'?
+      setWheelStyle(() => {return { transform: 'rotate(-30deg)' }}):
+      setWheelStyle(() => {return { transform: 'rotate(0deg)' }});
+  }
+
   return (
     <div id='app'>
-      <img id='board' className='image' src={board} alt='' />
-      <img id='wheel' className='image' src={wheel} alt='' />
+      <img 
+        id='board' 
+        className='image'
+        src={board} 
+        alt='' 
+      />
+      <img 
+        onClick={rotateWheel}
+        style={wheelStyle} 
+        id='wheel' 
+        className='image' 
+        src={wheel}
+        alt='' 
+      />
     </div>
   );
 }
