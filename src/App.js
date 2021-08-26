e5a3433cf6c1089039b9e7d672cfd46387868306
MeetCoding <meet.prakash.shah@gmail.com>
2021-08-26 13:20:32 +0530

    created the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import board from './board.svg';
+import wheel from './wheel.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div id='app'>
+      <img id='board' className='image' src={board} alt='' />
+      <img id='wheel' className='image' src={wheel} alt='' />
     </div>
   );
 }
